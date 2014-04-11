00ff2c44e1d26bab3a76f77983f69b853b76c6e1
Erick Dransch <ekgdrans@uwaterloo.ca>
2014-04-11 08:34:08 -0400

    add glm as a dep

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <glm/vec3.hpp>
 
 int main(){
     std::cout<<"HELLO, WORLD"<<std::endl;
     auto x = 15;
     std::cout<<x<<std::endl;
+
+    auto y = glm::vec3(0.0f, 0.1f, 0.4f);
+    std::cout<<"DONE"<<std::endl;
 }
