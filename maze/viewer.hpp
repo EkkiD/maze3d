7f38dcb0c5cc20988e0ccf3ba70a56babe6c17cb
Erick Dransch <ekgdrans@uwaterloo.ca>
2014-11-26 22:40:28 -0500

    separate shaders

--- maze/viewer.hpp
+++ maze/viewer.hpp
@@ -8,6 +8,7 @@
 #include <maze/common.hpp>
 #include <maze/interaction.hpp>
 #include <maze/generator.hpp>
+#include <chrono>
 
 constexpr static const GLfloat g_vertex_buffer_data[] = {
     -1.0f, -1.0f, 0.0f,
